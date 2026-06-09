0fd3fbea2f7029946457707ac323bae45ebb56ee
dharika <sreedharika32@gmail.com>
2026-06-09 21:19:53 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,36 +1,73 @@
 function toggleMenu() {
-    document.getElementById("navMenu").classList.toggle("active");
+
+document
+.getElementById("navMenu")
+.classList.toggle("active");
+
 }
 
 function scrollProjects() {
-    document.getElementById("projects").scrollIntoView({ behavior: "smooth" });
+
+document
+.getElementById("projects")
+.scrollIntoView({
+behavior:"smooth"
+});
+
 }
 
 function downloadResume() {
-    const link = document.createElement("a");
-    link.href = "resume.pdf";
-    link.target = "_blank";
-    link.click();
+
+window.open(
+"resume.pdf",
+"_blank"
+);
+
 }
 
 function sendMail() {
-    window.open("mailto:dharikashanmugam@gmail.com", "_blank");
+
+window.open(
+"https://mail.google.com/mail/?view=cm&fs=1&to=dharikashanmugam@gmail.com",
+"_blank"
+);
+
 }
 
-function openGithub() {
-    window.open("https://github.com/dharika-28", "_blank");
+function openGithub(){
+
+window.open(
+"https://github.com/dharika-28",
+"_blank"
+);
+
 }
 
-window.addEventListener("scroll", function () {
-    const btn = document.getElementById("topBtn");
+window.onscroll = function(){
 
-    if (document.documentElement.scrollTop > 300) {
-        btn.style.display = "block";
-    } else {
-        btn.style.display = "none";
-    }
+let btn =
+document.getElementById("topBtn");
+
+if(
+document.documentElement.scrollTop > 300
+){
+
+btn.style.display = "block";
+
+}
+else{
+
+btn.style.display = "none";
+
+}
+
+};
+
+function goTop(){
+
+window.scrollTo({
+top:0,
+behavior:"smooth"
 });
 
-function goTop() {
-    window.scrollTo({ top: 0, behavior: "smooth" });
 }
